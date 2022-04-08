5321575b4554bf902c4471a600a299502fad5c8d
qinliaoming <qinliaoming@163.com>
2022-04-08 22:45:25 +0800

    [modify]update StringUtils

--- src/main/java/carcinus/code/common/utils/StringUtils.java
+++ src/main/java/carcinus/code/common/utils/StringUtils.java
@@ -2,13 +2,30 @@ package carcinus.code.common.utils;
 
 public class StringUtils {
 
-    public static boolean isBlank(String str) {
-        return str == null || str.length() == 0;
+    public static boolean isBlank(String arg) {
+        return arg == null || arg.length() == 0;
     }
 
-    public static boolean noBlank(String str) {
-        return !isBlank(str);
+    public static boolean noBlank(String arg) {
+        return !isBlank(arg);
     }
 
+    public static boolean isBlanks(String... args) {
 
+        for (String arg : args) {
+            if (noBlank(arg)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean noBlanks(String... args) {
+
+        for (String arg : args) {
+            if (isBlank(arg)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
